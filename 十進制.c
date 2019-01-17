973e1ec90be7a7e2f7a4d5d4eab998c2a18edb7a
smile021020 <46781145+smile021020@users.noreply.github.com>
2019-01-18 03:02:45 +0800

    Add files via upload

---
+++ 十進制.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+	int input=0,Bin[8];
+
+	scanf("%d",&input);
+	if(input<0)
+        input+=256;                 //負數shift到正數
+
+	for(i=7;i>=0;i--)               //因為最後那項是我們二進位的第一個
+    {
+    	Bin[i]=input%2;             //慢慢存入0或1
+    	input/=2;                   //需要他的商繼續往下算
+    }
+    for(i=0;i<8;i++)
+    {
+    	if(i==7)
+            printf("%d\n",Bin[i]);
+            //cout<<Bin[i]<<endl;     //若為最後一位則換行
+    	else
+            printf("%d",Bin[i]);
+            //cout<<Bin[i];//否則緊接在後輸出
+    }
+	return 0;
+}
